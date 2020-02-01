9a0a0c9f9ebd684fdf9835ca4b698c50e34cdec4
annihargus <anni@hargus.de>
2020-02-01 16:06:12 +0100

    Added CharacterController

---
+++ GGJ Repair/Assets/Scripts/Added/CameraLock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLock : MonoBehaviour
+{
+    public GameObject Character;
+    public float Speed;
+    public float Sensitivity = 5.0f;
+    public float Smoothing = 2.0f;
+    public Vector2 SmoothingVector;
+    private Vector2 CurrentMouseLook;
+
+    private void Start()
+    {
+        //Character = this.gameObject;
+    }
+
+    void Update()
+    {
+        Vector2 MouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        MouseInput = Vector2.Scale(MouseInput, new Vector2(Smoothing * Sensitivity, Smoothing * Sensitivity));
+        SmoothingVector.x = Mathf.Lerp(SmoothingVector.x, MouseInput.x, 1f / Smoothing);
+        SmoothingVector.y = Mathf.Lerp(SmoothingVector.y, MouseInput.y, 1f / Smoothing);
+        CurrentMouseLook += SmoothingVector;
+
+        //transform.localRotation = Quaternion.AngleAxis(-CurrentMouseLook.y, Vector3.right);
+        //Character.transform.localRotation = Quaternion.AngleAxis(MouseInput.x, Character.transform.up);
+
+        transform.localRotation = Quaternion.AngleAxis(-CurrentMouseLook.y, Vector3.right);
+        Character.transform.localRotation = Quaternion.AngleAxis(CurrentMouseLook.x, Character.transform.up);
+    }
+}
